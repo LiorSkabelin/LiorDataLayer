235a43819e1eacb36c5c3b63a7efd786cf016685
Lior <Lior@LIORLEPTOP>
2023-10-30 16:16:51 +0200

    Add project files.

---
+++ ViveModel/UserDB.cs
@@ -0,0 +1,45 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ViveModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.ID = int.Parse(reader["id"].ToString());
+            user.Firstname = reader["firstName"].ToString();
+            user.Lastname = reader["lastName"].ToString();
+            user.Email = reader["email"].ToString();
+            user.Password = reader["password"].ToString();
+            user.Gender = bool.Parse(reader["gender"].ToString());
+            user.IsManger = bool.Parse(reader["isManager"].ToString());
+            user.Bday = DateTime.Parse(reader["Bday"].ToString());
+            return user;
+        }
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tbUser";
+            UserList list = new UserList(ExecuteCommand());
+            return list;
+        }
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tbUser WHERE id=" + id;
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+    }
+}
